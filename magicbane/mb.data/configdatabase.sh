2b08c8ae8fd06fce23466edb93835ac6c2965add
Ghodric1315 <166894413+Ghodric1315@users.noreply.github.com>
2024-04-14 02:39:38 -0400

    4
    4

--- magicbane/mb.data/configdatabase.sh
+++ magicbane/mb.data/configdatabase.sh
@@ -3,7 +3,7 @@
 service  mysql start
 
 mysql -u root -e "SET GLOBAL log_bin_trust_function_creators = 1;"
-mysql -u root -e "CREATE USER 'magicbox'@'localhost' IDENTIFIED BY 'ArtyomWasHere';"
+mysql -u root -e "CREATE USER 'Ghodric'@'localhost' IDENTIFIED BY 'Admin';"
 mysql -u root -e "CREATE DATABASE magicbane CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;"
 mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'magicbox'@'localhost';"
 mysql -u root -e "FLUSH PRIVILEGES;"
